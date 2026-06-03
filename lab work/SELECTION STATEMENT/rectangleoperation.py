f7b65d6d781d37a3526b572c82e24a6cc2599308
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-03 14:59:17 +0530

    wirite a program to find the area and perimeter of rectangle (if validate)

---
+++ lab work/SELECTION STATEMENT/rectangleoperation.py
@@ -0,0 +1,23 @@
+#wirite a program to find the area and perimeter of rectangle (if validate)
+#INPUT OF LENGTH AND BREADTH OF THE RECTANGLE
+print("-----RECTANGLE AREA AND PERIMETER CALCULATOR-----")
+len = float(input("Enter the length of the rectangle: "))
+bre = float(input("Enter the breadth of the rectangle: "))
+#-----------------------------------------
+print("----------------------------------------")
+print("length: ", len,"cm")
+print("breadth: ", bre,"cm")
+print("----------------------------------------")
+#VALIDATION OF THE LENGTH AND BREADTH
+if(len<0 or bre<0):
+    exit("length and breadth cannot be negative.....EXITED")
+#-----------------------------------------
+#CALCULATING THE PERIMETER OF THE RECTANGLE
+perimeter = 2 * (len+bre)
+#CALCULATING THE AREA OF THE RECTANGLE
+area = len * bre
+#-----------------------------------------
+print("perimeter of the rectangle is: ", perimeter,"cm")
+print("area of the rectangle is : ", area,"sq.cm")
+#-----------------------------------------
+print("----------------------------------------")
