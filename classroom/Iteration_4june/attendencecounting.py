48216fee60818f4080fa7a07bef4201657350be6
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-04 11:26:17 +0530

    iterator

--- classroom/Iteration_4june/attendencecounting.py
+++ classroom/Iteration_4june/attendencecounting.py
@@ -3,14 +3,29 @@
 
 #--------------------------------
 student = 1
-while(student <= 30):
-    attendence = input("Is student present or absent? (P/A) : ")
-    if(attendence == 'P' or attendence == 'p'):
+present_count = 0
+absent_count = 0
+
+while student <= 30:
+    attendance = input("Is student present or absent? (P/A): ")
+
+    if attendance == 'P' or attendance == 'p':
         print("Student", student, "is present.")
-    elif(attendence == 'A' or attendence == 'a'):
+        present_count += 1
+
+    elif attendance == 'A' or attendance == 'a':
         print("Student", student, "is absent.")
+        absent_count += 1
+
     else:
-        print("Invalid input. Please enter 'P' for present or 'A' for absent.")
-    student +=1
-    print("--------------------------------")
+        print("Invalid input. Please enter 'P' or 'A'.")
+        continue
+
+    student += 1
+    print("-------------------")
+
+print("\nAttendance Summary")
+print("Total Present Students:", present_count)
+print("Total Absent Students:", absent_count)
+print("--------------------------------")
 
