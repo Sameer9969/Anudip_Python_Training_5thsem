75d4089376a8567569a6adc41fe6a164d7bad334
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-05 22:35:16 +0530

    Problem Statement
    10 players ke scores input lo aur unme se sabse zyada score karne wale player ko identify karo.
    
    Requirements
    User se 10 players ke scores input lo aur unhe ek list me store karo.
    Sabhi players ke scores display karo.
    Loop ka use karke maximum score find karo (built-in max() function ka use nahi karna hai).
    Maximum score kis player ne banaya hai, uska player number bhi display karo.
    Output me maximum score aur player number print karo.
    Sample Input

--- classroom/list_5th/best_player_score.py
+++ classroom/list_5th/best_player_score.py
@@ -1,8 +1,19 @@
-player = []
+# Problem Statement
 
+# 10 players ke scores input lo aur unme se sabse zyada score karne wale player ko identify karo.
+
+# Requirements
+# User se 10 players ke scores input lo aur unhe ek list me store karo.
+# Sabhi players ke scores display karo.
+# Loop ka use karke maximum score find karo (built-in max() function ka use nahi karna hai).
+# Maximum score kis player ne banaya hai, uska player number bhi display karo.
+# Output me maximum score aur player number print karo.
+#list formation
+player = []
+#taking value in list
 for i in range(1, 11):
-    score = int(input("Enter score of player: "))
-    player.append(score)
+    scores = int(input("Enter score of player: "))
+    player.append(scores)
 
 print("Scores of players:", player)
 
