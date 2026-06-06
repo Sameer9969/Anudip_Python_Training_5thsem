bf93dd9db3a4e1e9b18c0fe2a152f13c1eb6c912
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-06 15:34:54 +0530

    demo

---
+++ classroom/dictionary_6thjune/studentdata.py
@@ -0,0 +1,5 @@
+#to create a dictionary to store student data
+student_data = {'std101':"akash",'std102': "abhinav",'std103' : "anil",'std104': "rahul"}
+# to display it
+print(student_data)
+print("-------------------------------")
