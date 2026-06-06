b3a40f196ddf835682ec7466a7cc83f6f9f50566
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-06 15:55:12 +0530

    Create Attendance tracker of 30 students. Ask the user to input roll number of student and also input whether student is Present or Absent. Store the data in dictionary where roll number will be used as a key and Attendance as Value. Display the roll number of students who are Present

---
+++ classroom/dictionary_6thjune/attendence_tracker.py
@@ -0,0 +1,21 @@
+"""Create Attendance tracker of 30 students. Ask the user to input roll number of student and also 
+input whether student is Present or Absent. Store the data in dictionary where roll number will 
+be used as a key and Attendance as Value. 
+Display the roll number of students who are Present"""
+
+
+attendance = {}
+
+# Input attendance of 30 students
+for i in range(1, 5):
+    roll_no = int(input("Enter Roll Number: "))
+    status = input("Present or Absent (P/A): ")
+
+    attendance[roll_no] = status
+
+# Display present students
+print("\nStudents who are Present:")
+
+for roll_no in attendance:
+    if attendance[roll_no] == "P" or attendance[roll_no] == "p":
+        print(roll_no)
