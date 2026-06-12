d9aa9e9a40d105bb6192ce908304f42e4e27c56c
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-12 11:10:30 +0530

    Problem 11: Student Feedback Analysis System Problem Statement A training institute collects feedback from students after completing a Python course. The feedback comments are stored in a text file named feedback.txt. Sample Input/Data (feedback.txt) The sessions were very interactive and informative. Excellent teaching methodology and practical examples. The pace of the course was appropriate. More real-world projects should be included. The trainer explained concepts very clearly. Tasks 1. Count the total number of lines. 2. Count the total number of words. 3. Count the total number of characters. 4. Find the longest feedback comment. 5. Find the shortest feedback comment. 6. Count the total number of vowels present in the file.

---
+++ classroom/python_test2/student_feedback.py
@@ -0,0 +1,32 @@
+"""Problem 11: Student Feedback Analysis System 
+Problem Statement 
+A training institute collects feedback from students after completing a Python course. The feedback 
+comments are stored in a text file named feedback.txt. 
+Sample Input/Data (feedback.txt) 
+The sessions were very interactive and informative. 
+Excellent teaching methodology and practical examples. 
+The pace of the course was appropriate. 
+More real-world projects should be included. 
+The trainer explained concepts very clearly. 
+Tasks 
+1. Count the total number of lines.  
+2. Count the total number of words.  
+3. Count the total number of characters.  
+4. Find the longest feedback comment.  
+5. Find the shortest feedback comment.  
+6. Count the total number of vowels present in the file.  
+Sample Output 
+Total Lines: 5 
+ 
+Total Words: 35 
+ 
+Total Characters: 220 
+ 
+Longest Feedback: 
+Excellent teaching methodology and practical examples. 
+ 
+Shortest Feedback: 
+The pace of the course was appropriate. 
+ 
+Total Vowels: 76"""
+
