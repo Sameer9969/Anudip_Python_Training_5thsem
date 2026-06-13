9e47dc24cb12b10f36242e6b928f3c5a2a856ad7
Sameer9969 <thakursameer.singhrajput@gmail.com>
2026-06-13 10:47:40 +0530

    area and perimeter of reactangle

---
+++ classroom/class_method/area_and_perimeter.py
@@ -0,0 +1,38 @@
+#area and perimeter of reactangle 
+class Rectangle:
+    def __init__(self,lenght,breath):
+        self.lenght=lenght
+        self.breath=breath
+
+    #area of rectangle
+    def area(self):
+        area = self.lenght*self.breath
+        print("area of rectangle is :",area,"sq.cm")
+    
+    #perimeter of rectangle
+    def perimeter(self):
+        perimeter = 2*(self.lenght+self.breath)
+        print("perimeter of rectangle is :",perimeter,"cm")
+
+#---------------------main manu-------------------------
+while(True):
+    print("---------main manu----------")
+    print(" 1. for area")
+    print(" 2. for perimeter")
+    print(" 3. for exit")
+
+    choice=float(input("enter your choice:"))
+
+    if choice==1:
+        lenght=float(input("enter lenght:"))
+        breath=float(input("enter breath:"))
+        rectangle=Rectangle(lenght,breath)
+        rectangle.area()
+
+    elif choice == 2:
+        lenght=float(input("enter lenght:"))
+        breath=float(input("enter breath:"))
+        rectangle = Rectangle(lenght,breath)
+        rectangle.perimeter()
+    else:
+        exit("exexted successfully")
